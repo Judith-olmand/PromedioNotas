f35b305f0b3a6c9a3f9953bd2afa87ac3ae26e57
Judith-olmand <judith.olmand@educa.jcyl.es>
2025-11-07 10:15:16 +0100

    añadida una constante NUM_NOTAS

--- src/PromedioNotas.java
+++ src/PromedioNotas.java
@@ -1,13 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public  class PromedioNotas {
+    public  static void main(String[] args) {
+        final int NUM_NOTAS = 5;
+        double[] notas = new double[NUM_NOTAS];
+        int inicio;
+        double calculadora = 0;
+
+        Scanner sc = new Scanner(System.in);
+        for (inicio = 0; inicio < notas.length; inicio++) {
+            System.out.println("Introduce una nota: ");
+            notas[inicio] = sc.nextDouble();
+            calculadora += notas[inicio];
+        }
+
+        double promedio = calculadora / NUM_NOTAS;
+        System.out.println("El promedio es: " + promedio);
+
+        if (promedio >= 9) {
+            System.out.println("El promedio es excelente");
+        } else if (promedio >= 7) {
+            System.out.println("Buen trabajo");
+        } else if (promedio >= 5) {
+            System.out.println("Necesitas mejorar");
+        }else {
+            System.out.println("Has suspendido");
+        }
+
+        for( double n: notas){
+            System.out.print(n + "; ");
+        }
+        System.out.println();
+
+        inicio = 0;
+        while (inicio < notas.length){
+            System.out.println("Nota" + (inicio+1) + ": " + notas[inicio]);
+            inicio++;
+        }
+
+        inicio = 0;
+        do {
+            System.out.print("Nota" + (inicio+1) + ": " + notas[inicio] + " ------ ");
+            inicio++;
+        }while (inicio < NUM_NOTAS);
+
+    }
 }
